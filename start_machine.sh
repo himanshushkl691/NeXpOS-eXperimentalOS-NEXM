5403f6e6890b9b241aa74552ff43a995bf88ba34
Himanshu Shukla <himanshushkl691@gmail.com>
2019-10-27 23:12:51 +0530

    Done and dusted!

--- start_machine.sh
+++ start_machine.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 curr=$(pwd)
 cd $HOME/Desktop/mynexpos/nexsm
-./xsm
+if [ -z $2 ]
+then
+	if [ -z $1 ]
+	then
+		./xsm
+	else
+		./xsm --timer $1
+	fi
+else
+	if [ -z $1 ]
+	then
+		./xsm --debug
+	else
+		./xsm --debug --timer $1
+	fi
+fi
 cd $curr
